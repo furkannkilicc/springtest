b6f133f5d3343a56245284d19f08b734ae720364
furkan <kilic.furkank@gmail.com>
2024-12-30 15:19:16 +0300

    ok

---
+++ src/main/java/com/example/furkantest/FurkantestApplication.java
@@ -0,0 +1,13 @@
+package com.example.furkantest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurkantestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurkantestApplication.class, args);
+	}
+
+}
